ac06fd66a632df694d95066d5476b0998dc64416
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-16 10:18:01 -0700

    implement useDarkMode hook into App component

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 
-export const useDarkMode = (initialValue) => {
+const useDarkMode = (initialValue) => {
     const [darkMode, setDarkMode] = useState(initialValue);
 
-    const toggleMode = updatedValue => {
-        setDarkMode(updatedValue);
-      };
-      return [darkMode, setDarkMode, toggleMode]
+      return [darkMode, setDarkMode]
 }
+
+export default useDarkMode;
